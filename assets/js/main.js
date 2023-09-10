514558e0bb8bd5c3fb3e4c49a73762499f35d29a
Ryotruuser <ryonantruuser@gmail.com>
2023-09-10 15:48:24 -0300

    finalizando estilos e comecando a setar utilizacao da Pokedex API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("sucesso")
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json()) //retornando objeto para o response
+    .then((jsonBody) => console.log(jsonBody)) //pegando objeto ja transformado
+    .catch((error) => console.error(error)) // caso de erro
